f5c1f01169fd288108625fb14237c713bbd19464
Wailakkad <akkadouail8@gmail.com>
2025-08-22 11:00:24 +0200

    new feature ClinicTaskManagmenetApp/routes/tasks.js

--- ClinicTaskManagmenetApp/controllers/tasksControllers.js
+++ ClinicTaskManagmenetApp/controllers/tasksControllers.js
@@ -34,3 +34,22 @@ exports.createTask = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 }
+
+
+exports.getTasksByUser = async (req, res) => {
+  try {
+    const userId = req.params.userId; // assuming you pass userId in the route like /tasks/:userId
+
+    const tasks = await Task.find({ assegnedTo: userId })
+      
+
+    if (!tasks || tasks.length === 0) {
+      return res.status(404).json({ message: 'No tasks found for this user' });
+    }
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
--- ClinicTaskManagmenetApp/routes/tasks.js
+++ ClinicTaskManagmenetApp/routes/tasks.js
@@ -1,11 +1,14 @@
 const experss = require('express');
 const router = experss.Router();
 const tasksController = require('../controllers/tasksControllers');
+const GetUserTask = require('../controllers/tasksControllers');
 
 // Middleware to check authentication
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Route to create a task
 router.post('/create', authMiddleware, tasksController.createTask);
+// Route to get tasks assigned to the logged-in user
+router.get('/my-tasks/:userId', GetUserTask.getTasksByUser);
 
 module.exports = router;
